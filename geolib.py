705b66fcdd3cce550a2586ecd355f1d1e4ed79c4
Xuehui Ye <yexuehui@momenta.ai>
2019-06-23 21:44:01 +0800

    geo

---
+++ geolib.py
@@ -0,0 +1,22 @@
+from math import radians, cos, sin, asin, sqrt
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    lon1 = float(lon1)
+    lon2 = float(lon2)
+    lat2 = float(lat2)
+    lat1 = float(lat1)
+    # 将十进制度数转化为弧度
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine公式
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # 地球平均半径，单位为公里
+    return c * r * 1000
