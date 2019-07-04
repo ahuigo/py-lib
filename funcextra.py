471569bce17c5d85124112fd5118d895239fbaeb
Xuehui Ye <yexuehui@momenta.ai>
2019-07-04 13:35:20 +0800

    func-iter2reduce-fix-bug

--- funcextra.py
+++ funcextra.py
@@ -1,78 +1,6 @@
 from functools import wraps
 import traceback
 '''
-
-arr = [
-        ['dev01', '1'], ['dev01', '2'], ['dev01', '3'],
-        ['dev02', '1'], ['dev02', '2'], ['dev02', '3'],
-        ['dev03', '1'], ['dev03', '2'], ['dev03', '3'],
-        ]
-for i in iter2(iter(arr), 0):
-    print(i)
-    for j in i:
-        print(j)
-'''
-def iter2(arr, index):
-    item = next(arr, None)
-    loop_end = True
-
-    def loop(arr):
-        nonlocal item
-        nonlocal loop_end
-        yield item
-        old_item = item
-        try:
-            while True:
-                item = next(arr, None)
-                if item is None or item[index] != old_item[index]:
-                    break
-                yield item
-        finally:
-            loop_end = True
-            
-    while item is not None and loop_end:
-        loop_end = False
-        yield loop(arr)
-
-'''
-arr = [
-    ['dev01', 10],['dev01', 12], ['dev01', 13],
-    ['dev01', 21],['dev01', 22], ['dev01', 23],
-]
-
-def compare(prev, curr):
-    return True if prev[1] - curr[1]>2 else False
-
-for i in iter2reduce(iter(arr), compare):
-    for j in i:
-        print(j)
-
-'''
-def iter2reduce(arr, compare=None, return_end=True):
-    item = next(arr, None)
-    iter1_end = True
-
-    def iter1(arr):
-        nonlocal item
-        nonlocal iter1_end
-        try:
-            while True:
-                old_item = item
-                item = next(arr, None)
-                if item is None or (compare and compare(old_item, item)):
-                    if return_end:
-                        yield old_item, None
-                    break
-                yield old_item,item
-        finally:
-            iter1_end = True
-            
-    while item is not None and iter1_end:
-        iter1_end = False
-        #logging.debug(item)
-        yield iter1(arr)
-
-'''
 Retry function/method
 '''
 def retry(howmany=2, raiseError=True):
@@ -93,3 +21,69 @@ def retry(howmany=2, raiseError=True):
     return tryIt
 
 
+
+'''
+分组迭代
+arr = [
+        ['dev01', '1'], ['dev01', '2'], ['dev01', '3'],
+        ['dev02', '1'], ['dev02', '2'], ['dev02', '3'],
+        ['dev03', '1'], ['dev03', '2'], ['dev03', '3'],
+        ]
+for i in iterChunk(iter(arr), 0):
+    print(i)
+    for j in i:
+        print(j)
+
+arr = [
+    ['dev01', 10],['dev01', 12], ['dev01', 13],
+    ['dev01', 21],['dev01', 22], ['dev01', 23],
+]
+
+def compare(prev, curr):
+    return True if prev[1] - curr[1]>2 else False
+
+for i in iterChunk(iter(arr), compare):
+    print(i)
+    for j in i:
+        print(j)
+
+'''
+def iterChunk(arr, index=None, compare = None):
+    item = next(arr, None)
+    iter1_end = True
+
+    def iter1(arr):
+        nonlocal item
+        nonlocal iter1_end
+        try:
+            while True:
+                yield item
+                old_item = item
+                item = next(arr, None)
+                if item is None \
+                    or (index and item[index] != old_item[index]) \
+                    or (compare and compare(old_item, item)):
+                    break
+        finally:
+            iter1_end = True
+            
+    while item is not None and iter1_end:
+        iter1_end = False
+        yield iter1(arr)
+'''
+配对迭代
+'''
+def iterReduce(arr, return_end=True):
+    item = next(arr, None)
+    if item is None:
+        return
+
+    while True:
+        old_item = item
+        item = next(arr, None)
+        if item is None:
+            if return_end:
+                yield old_item, None
+            break
+        yield old_item,item
+            
