7cbde3c356f64846425cf6f04726fd31f4e41d0e
ahuigo <ahuigo@ahuigo.com>
2021-11-19 19:19:54 +0800

    ldap

--- account/ldap.py
+++ account/ldap.py
@@ -2,10 +2,15 @@ import ldap3, base64
 
 LDAP_CONFIG = {
     "ldap_host": "ldap://192.168.1.10:389",
-    "base_dn": "OU=All Users,DC=ahuigo,DC=com",
-    "scope": "users",
-    "ldap_pass": "passwd!",
     "bind_dn":"CN=<department>,OU=Account,DC=ahuigo,DC=com",
+    "bind_pass": "passwd!",
+    "base_dn": "OU=All Users,DC=ahuigo,DC=com",
+    "search_user_filter": '(&(objectclass=person)(CN=%s))',
+    # pgadmin:
+    #   LDAP_SEARCH_FILETER='(objectclass=person)'
+    #   LDAP_USERNAME_ATTRIBUTE='CN'
+    #   self.username=person
+    "attributes":['sAMAccountName'] #"scope": "users",
 }
 
 class AD(object):
@@ -14,7 +19,7 @@ class AD(object):
         self.ldap_host = LDAP_CONFIG.get('ldap_host')
         self.base_dn = LDAP_CONFIG.get('base_dn')
         self.bind_dn = LDAP_CONFIG.get('bind_dn')
-        self.ldap_pass = LDAP_CONFIG.get('ldap_pass')
+        self.bind_pass = LDAP_CONFIG.get('bind_pass')
         self._server = self._get_server()
 
 
@@ -23,19 +28,19 @@ class AD(object):
 
     def _get_connection(self):
         if not self._connection:
-            self._connection = ldap3.Connection(self._server, self.bind_dn, self.ldap_pass, auto_bind=True)
+            self._connection = ldap3.Connection(self._server, self.bind_dn, self.bind_pass, auto_bind=True)
 
         else:
             try:
                 self._connection.rebind() 
             except ldap3.core.exceptions.LDAPSocketSendError as e:
-                self._connection = ldap3.Connection(self._server, self.bind_dn, self.ldap_pass, auto_bind=True)
+                self._connection = ldap3.Connection(self._server, self.bind_dn, self.bind_pass, auto_bind=True)
                 
         return self._connection
 
     def login(self, username, passwd):
         connection = self._get_connection()
-        search_res = connection.search(self.base_dn, '(&(objectclass=person)(CN=%s))' % username, attributes=[LDAP_CONFIG.get('scope')])
+        search_res = connection.search(self.base_dn, '(&(objectclass=person)(CN=%s))' % username, attributes=LDAP_CONFIG.get('attributes'))
 
         if not search_res:
             return 'user_not_exists'
