04a71719e9db93a5bda7cb05e6478d6579c3a360
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-10-01 00:14:16 +0800

    stdio-tty-input

--- os/stdio/stdio.py
+++ os/stdio/stdio.py
@@ -1,13 +1,24 @@
 import fileinput,sys
 #for line in fileinput.input(): pass
+'''
+#只读取一行
+q=input() 
 
-
+# 读取多行(ctrl+D)
+sys.stdin.read() 
+open(0).read() 
+'''
 print(sys.stdout,sys.stdin)
 
 if __name__=="__main__":
     # read
-    print("read:",sys.stdin.read())
-    print("read:",open(0).read())
+    if sys.stdin.isatty:
+        # read from tty(ctrl+D quit)
+        print("read tty:",sys.stdin.read())
+        print("read tty:",open(0).read())
+    else:
+        #read from pipe
+        print("read pipe:",sys.stdin.read())
     # write
     out="out"
     print(sys.stdout.write(out))
