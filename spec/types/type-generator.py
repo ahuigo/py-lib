b866172d46dab301ff61ee6fa46dd82cf2473b89
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-09-30 23:57:46 +0800

    type

--- spec/types/type-generator.py
+++ spec/types/type-generator.py
@@ -3,6 +3,7 @@
 LiteralString | str 是生成器产生的值的类型。这表示生成器可以产生 LiteralString 类型或 str 类型的值。
 Any 是生成器接收的值的类型。这表示你可以发送任何类型的值给生成器（使用 generator.send(value) 方法）。
 '''
+from typing import Generator, LiteralString, Any
 def getQuery()->Generator[LiteralString | str, Any, None]:
     query = 'abc '
     query = query.strip()
